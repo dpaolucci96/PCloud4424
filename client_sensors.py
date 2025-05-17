289b3380d64e8ae0aa6c811d506664f9f4e5f027
dpaolucci96 <debora.paolucci@outlook.it>
2025-05-17 11:33:34 +0200

    Primo commit

---
+++ client_sensors.py
@@ -0,0 +1,17 @@
+from requests import get, post
+import time
+
+base_url = 'http://localhost:80'
+sensor = 's1'
+#leggo il file
+with open('CleanData_PM10.csv') as f:
+    #leggo tutte le righe del file e salto la prima riga (intestazione)
+    for l in f.readlines()[1:]:
+        data,val = l.strip().split(',')
+        print(data,val)
+        r = post(f'{base_url}/sensors/{sensor}',
+                 data={'data':data,'val':val})
+        time.sleep(5) # ogni 5 secondi effettua chiamata
+
+
+print('done')
